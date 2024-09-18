2c9737ac680d29ab766271d8c86b659346bdec25
jydnbach <jaydenkim95@gmail.com>
2024-09-18 16:49:39 +0900

    http post

--- Part 0/frontpgagehtml.js
+++ Part 0/frontpgagehtml.js
@@ -19,3 +19,14 @@ app.get('/', (req, res) => {
   const page = getFrontPageHtml(notes.length);
   res.send(page);
 });
+
+// POST request practice
+
+app.post('/new_note', (req, res) => {
+  notes.push({
+    content: req.body.note,
+    date: new Date(),
+  });
+
+  return res.redirect('/notes');
+});

af7e0eb99763c9a0fae94a1ec5e120834fcd565a
jydnbach <jaydenkim95@gmail.com>
2024-09-20 16:45:54 +0900

    exercise 1.2

--- Part 1/part-1-exercises/src/App.jsx
+++ Part 1/part-1-exercises/src/App.jsx
@@ -42,14 +42,9 @@ const App = () => {
     <div>
       <Header course={course} />
 
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3} />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}

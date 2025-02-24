8878b6a2162257c23101b976c527f7bfc8b8929f
jydnbach <jaydenkim95@gmail.com>
2025-02-24 17:16:44 +0700

    refactor: objects and arrays

--- Part 1/part-1-exercises/src/Content.jsx
+++ Part 1/part-1-exercises/src/Content.jsx
@@ -1,14 +1,14 @@
 import Part from "./Part";
 
 const Content = ({
-part1, part2, part3
+parts
 }) => {
 
   return (
     <div>
-      <Part part={part1.name} exercise={part1.exercises}/>
-      <Part part={part2.name} exercise={part2.exercises}/>
-      <Part part={part3.name} exercise={part3.exercises}/>
+      <Part part={parts[0].name} exercise={parts[0].exercises}/>
+      <Part part={parts[1].name} exercise={parts[1].exercises}/>
+      <Part part={parts[2].name} exercise={parts[2].exercises}/>
     </div>
   );
 };
--- Part 1/part-1-exercises/src/Total.jsx
+++ Part 1/part-1-exercises/src/Total.jsx
@@ -1,6 +1,6 @@
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+const Total = ({parts }) => {
+  return <p>Number of exercises {parts[0].exercises1 + parts[1].exercises2 + parts[2].exercises3}</p>;
 };
 
 export default Total;

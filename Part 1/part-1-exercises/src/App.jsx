493590c0c2e46dee64f555cd9f5e1b49d007b9b7
jydnbach <jaydenkim95@gmail.com>
2025-02-25 14:11:49 +0700

    button statistics

--- Part 1/part-1-exercises/src/App.jsx
+++ Part 1/part-1-exercises/src/App.jsx
@@ -1,23 +1,54 @@
-import Header from "./Header";
-import Content from "./Content"
-import Total from "./Total"
+import { useState } from "react";
+// import Header from "./Header";
+// import Content from "./Content"
+// import Total from "./Total"
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-       { name: "Fundamentals of React", exercises: 10},
-       { name: "Using props to pass data", exercises: 7},
-       { name: "State of a component", exercises: 14}
-     ]
+
+  // const course = {
+  //   name: 'Half Stack application development',
+  //   parts: [
+  //      { name: "Fundamentals of React", exercises: 10},
+  //      { name: "Using props to pass data", exercises: 7},
+  //      { name: "State of a component", exercises: 14}
+  //    ]
+  // }
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  
+  const handleGood = () => {
+    return setGood(good + 1)
   }
-  
-  
+  const handleNeutral = () => {
+    return setNeutral(neutral + 1)
+  }
+  const handleBad = () => {
+    return setBad(bad + 1)
+  }
+
   return (
     <div>
-      <Header course={course.name} />
+      <div>
+        <h2>give feedback</h2>
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button>
+        <div>
+
+        <h2>statistics</h2>
+        <p>{good}</p>
+        <p>{neutral}</p>
+        <p>{bad}</p>
+        </div>
+        
+      </div>
+
+      {/* <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Total parts={course.parts} /> */}
     </div>
   );
 };

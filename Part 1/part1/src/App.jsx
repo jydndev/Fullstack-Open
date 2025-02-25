8b351a7f46d90243d9fdb65ce7874f0f81b5b2d6
jydnbach <jaydenkim95@gmail.com>
2025-02-25 11:45:22 +0700

    handling arrays, async state updates

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,28 +1,38 @@
 import { useState } from "react";
 // import Display from "./Display";
 // import Button from "./Button";
-
 const App = () => {
-const [clicks, setClicks] = useState({
-  left: 0, right: 0
-})
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-const handleLeftClick = () => {
-  setClicks({...clicks, left: clicks.left + 1})
-}
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-const handleRightClick = () => {
-  setClicks({...clicks, right: clicks.right + 1})
-}
-  
-return (
-  <div>
-    {clicks.left}
-    <button onClick={handleLeftClick}>left</button>
-    <button onClick={handleRightClick}>right</button>
-    {clicks.right}
-  </div>
-)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    setTotal(left + right)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
+     <p>total {total}</p>
+    </div>
+  )
 }
 
 export default App;

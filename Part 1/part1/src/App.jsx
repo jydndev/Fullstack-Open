5a504bc6dc38a1d7014cded0d3f5f0fda02cd855
jydnbach <jaydenkim95@gmail.com>
2025-02-25 10:42:33 +0700

    complex state : left and right

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
-import Display from "./Display";
-import Button from "./Button";
+// import Display from "./Display";
+// import Button from "./Button";
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+const [clicks, setClicks] = useState({
+  left: 0, right: 0
+})
 
-  const increaseByOne = () => setCounter(counter + 1)
+const handleLeftClick = () => {
+  const newClicks = {
+    left: clicks.left + 1,
+    right: clicks.right
+  }
+  setClicks(newClicks)
+}
 
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-
-  return (
-    <div>
-      <Display counter={counter}/>
-
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
-    </div>
-  )
+const handleRightClick = () => {
+  const newClicks = {
+    left: clicks.left ,
+    right: clicks.right + 1
+  }
+  setClicks(newClicks)
+}
+  
+return (
+  <div>
+    {clicks.left}
+    <button onClick={handleLeftClick}>left</button>
+    <button onClick={handleRightClick}>right</button>
+    {clicks.right}
+  </div>
+)
 }
 
 export default App;

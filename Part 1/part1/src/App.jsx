6b6d6a1fd761f79f39634623ae8e7e44c4e20347
jydnbach <jaydenkim95@gmail.com>
2025-02-25 10:22:58 +0700

    rectoring - compact component

--- Part 1/part1/src/App.jsx
+++ Part 1/part1/src/App.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import Display from "./Display";
+import Button from "./Button";
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-
       <Display counter={counter}/>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}> 
-        zero
-      </button>
+
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }

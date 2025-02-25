961cb9716d0de33f7c38545873c10e10d1a8f130
jydnbach <jaydenkim95@gmail.com>
2025-02-25 10:23:56 +0700

    button component refactor

--- Part 1/part1/src/Button.jsx
+++ Part 1/part1/src/Button.jsx
@@ -1,9 +1,8 @@
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
+const Button = ({onClick, text}) =>  (
+    <button onClick={onClick}>
+      {text}
     </button>
   )
-}
+
 
 export default Button

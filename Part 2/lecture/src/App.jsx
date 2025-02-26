653db8cc254de63f52a85704132a2793ac0bbbaf
jydnbach <jaydenkim95@gmail.com>
2025-02-26 21:41:04 +0700

    add note feature

--- Part 2/lecture/src/App.jsx
+++ Part 2/lecture/src/App.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import Note from './components/Note';
 
-const App = ({ notes }) => {
-  // const { notes } = props;
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log('button clicked', event.target);
+  };
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <ul>
-          {notes.map((note) => (
-            <Note key={note.id} note={note} />
-          ))}
-        </ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };

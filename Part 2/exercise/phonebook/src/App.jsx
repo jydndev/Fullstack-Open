2bf6ecf5fd50a405f5c470a7bec83dbdeb77a5b7
jydnbach <jaydenkim95@gmail.com>
2025-02-26 23:00:13 +0700

    search function

--- Part 2/exercise/phonebook/src/App.jsx
+++ Part 2/exercise/phonebook/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
+
   const [newName, setNewName] = useState('');
   const [newNum, setNewNum] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,9 +37,22 @@ const App = () => {
     setNewNum(e.target.value);
   };
 
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.includes(filter)
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with
+        <input onChange={handleFilter} />
+      </div>
 
       <form onSubmit={handleSubmit}>
         <div>
@@ -48,7 +68,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => {
+        {filteredPersons.map((person) => {
           return (
             <li key={person.name}>
               {person.name} {person.number}

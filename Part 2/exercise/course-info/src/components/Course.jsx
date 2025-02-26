c028b93d9199e9d7b337593d303b87b407c4a09f
jydnbach <jaydenkim95@gmail.com>
2025-02-26 15:33:02 +0700

    total calc

--- Part 2/exercise/course-info/src/components/Course.jsx
+++ Part 2/exercise/course-info/src/components/Course.jsx
@@ -1,11 +1,17 @@
 import Content from './Content';
 import Header from './Header';
+import Total from './Total';
 
 const Course = ({ course }) => {
+  const totalCalc = () => {
+    return course.parts.reduce((sum, part) => sum + part.exercises, 0);
+  };
+
   return (
     <div>
       <Header name={course.name} />
       <Content part={course.parts} />
+      <Total total={totalCalc()} />
     </div>
   );
 };

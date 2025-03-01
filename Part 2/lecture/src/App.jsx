22fbde8bc8dfcc076176572422f17c6df50a83ea
jydnbach <jaydenkim95@gmail.com>
2025-03-01 09:35:43 +0700

    footer componenet

--- Part 2/lecture/src/App.jsx
+++ Part 2/lecture/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Note from './components/Note';
 import Notification from './components/Notification';
 import noteService from './services/notes';
+import Footer from './components/Footer';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -76,6 +77,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };

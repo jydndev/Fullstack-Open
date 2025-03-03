9368cddb2f656456974746fd9998beb3ea7de71f
jydnbach <jaydenkim95@gmail.com>
2025-03-03 21:00:32 +0700

    error handling

--- Part 3/index.js
+++ Part 3/index.js
@@ -27,6 +27,17 @@ app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
 // const app = http.createServer((request, response) => {
 //   response.writeHead(200, { 'Content-Type': 'application/json' });
 //   response.end(JSON.stringify(notes));

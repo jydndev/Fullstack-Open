b81e26870fd8a605aa442097c413c6143cf02e0d
jydnbach <jaydenkim95@gmail.com>
2025-03-04 18:22:32 +0700

    different approach

---
+++ Part 2/exercise/data-for-countries/src/App2.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import CountryList from './components/CountryList';
+import axios from 'axios';
+
+const COUNTRY_API_URL = 'https://studies.cs.helsinki.fi/restcountries/';
+
+const App2 = () => {
+  const [search, setSearch] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${COUNTRY_API_URL}/api/all`)
+      .then((res) => setCountries(res.data));
+  }, []);
+
+  const searchedCountries = countries.filter((c) => {
+    c.name.common.toLowerCase().includes(search.toLocaleLowerCase());
+  });
+
+  return (
+    <>
+      <div>
+        find countries
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {search === '' ? null : (
+        <CountryList countries={searchedCountries} showCountry={setSearch} />
+      )}
+    </>
+  );
+};
+
+export default App2;

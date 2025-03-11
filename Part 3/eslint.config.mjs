5ff034ddca7d7f2e0f589ab201436cd79c2c7628
jydnbach <jaydenkim95@gmail.com>
2025-03-11 13:51:59 +0700

    3.9 implement frontend

--- Part 3/eslint.config.mjs
+++ Part 3/eslint.config.mjs
@@ -20,7 +20,7 @@ export default [
       '@stylistic/js/indent': ['error', 2],
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
+      // '@stylistic/js/semi': ['error', 'never'],
       eqeqeq: 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],

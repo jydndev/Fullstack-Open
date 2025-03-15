cac9547d42232ed72dea85f00887230742ea5b21
jydnbach <jaydenkim95@gmail.com>
2025-03-15 11:08:00 +0700

    ..

--- Part 3/controllers/notes.js
+++ Part 3/controllers/notes.js
@@ -1,23 +1,42 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 
-notesRouter.get('/', (req, res) => {
-  Note.find({}).then((notes) => res.json(notes));
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find({});
+  res.json(notes);
 });
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        res.json(note);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((err) => next(err));
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
 });
 
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({ error: 'Content is missing' }).end();
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  const savedNote = await note.save();
+  res.status(201).json(savedNote);
+});
+
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
+
+notesRouter.put('/:id', async (req, res, next) => {
   const body = req.body;
 
   const note = new Note({
@@ -25,33 +44,14 @@ notesRouter.post('/', (req, res, next) => {
     important: body.important || false,
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      res.json(savedNote);
-    })
-    .catch((err) => next(err));
-});
-
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id)
-    .then(() => res.status(204).end())
-    .catch((err) => next(err));
-});
-
-notesRouter.put('/:id', (req, res, next) => {
-  const body = req.body;
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  });
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
-    .then((updatedNote) => {
-      res.json(updatedNote);
-    })
-    .catch((err) => next(err));
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, {
+      new: true,
+    });
+    res.json(updatedNote);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = notesRouter;

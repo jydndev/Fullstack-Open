293b114768168621e3e197831d73211b878ef8cf
jydnbach <jaydenkim95@gmail.com>
2025-03-18 17:33:11 +0700

    ..

--- Part 3/controllers/notes.js
+++ Part 3/controllers/notes.js
@@ -1,6 +1,7 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({}).populate('user', {
@@ -19,10 +20,24 @@ notesRouter.get('/:id', async (req, res) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
+
 notesRouter.post('/', async (req, res) => {
   const body = req.body;
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   if (!body.content) {
     return res.status(400).json({ error: 'Content is missing' }).end();

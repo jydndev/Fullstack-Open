190532be652961051a46ddb43eddcdf6b5ac2086
jydnbach <jaydenkim95@gmail.com>
2025-03-19 16:37:30 +0700

    part 5 frontend: login logic

--- Part 2/lecture/src/App.jsx
+++ Part 2/lecture/src/App.jsx
@@ -3,6 +3,7 @@ import Note from './components/Note';
 import Notification from './components/Notification';
 import noteService from './services/notes';
 import Footer from './components/Footer';
+import loginService from './services/login';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -11,6 +12,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -55,38 +57,66 @@ const App = () => {
     setNewNote(e.target.value);
   };
 
+  const loginForm = () => {
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUserName(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>;
+  };
+  const noteForm = () => {
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>;
+  };
+
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    console.log('logged in with', username, password);
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUserName('');
+      setPassword('');
+    } catch (err) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
   };
 
   return (
     <div>
       <h1>Notes</h1>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUserName(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+
       <Notification message={errorMessage} />
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          noteForm()
+        </div>
+      )}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -101,10 +131,7 @@ const App = () => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+
       <Footer />
     </div>
   );

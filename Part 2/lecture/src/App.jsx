83a74d7250a0bdb3e377a4e061e4a1897522241d
jydnbach <jaydenkim95@gmail.com>
2025-04-16 21:11:07 +0900

    save login details in local storage

--- Part 2/lecture/src/App.jsx
+++ Part 2/lecture/src/App.jsx
@@ -94,6 +94,8 @@ const App = () => {
     e.preventDefault();
     try {
       const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
       noteService.setToken(user.token);
       setUser(user);
       setUserName('');

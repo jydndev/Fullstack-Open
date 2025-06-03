83d4bbc6434f8fe8a84bbc0a2e188e0a906593fe
jydnbach <jaydenkim95@gmail.com>
2025-06-03 08:46:18 +0700

    2 inputs test

--- Part 2/lecture/src/components/NoteForm.jsx
+++ Part 2/lecture/src/components/NoteForm.jsx
@@ -22,7 +22,15 @@ const NoteForm = ({ createNote }) => {
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleChange} />
+        <input
+          value={newNote}
+          onChange={handleChange}
+          placeholder='write note content here'
+        />
+        <input
+          value={...}
+          onChange={...}
+        />    
         <button type="submit">save</button>
       </form>
     </div>

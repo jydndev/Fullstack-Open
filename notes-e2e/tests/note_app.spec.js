dee773f1067a5c002a8fa55a3ae96e314a81dcf4
jydnbach <jaydenkim95@gmail.com>
2025-06-04 08:26:34 +0700

    test - login form test text input

--- notes-e2e/tests/note_app.spec.js
+++ notes-e2e/tests/note_app.spec.js
@@ -1,15 +1,27 @@
 const { test, describe, expect } = require('@playwright/test');
 
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  // test('front page can be opened', async ({ page }) => {
+  //   await page.goto('http://localhost:5173');
+
+  //   const locator = await page.getByText('Notes');
+  //   await expect(locator).toBeVisible();
+  //   await expect(
+  //     page.getByText(
+  //       'Note app, Department of Computer Science, University of Helsinki 2024'
+  //     )
+  //   ).toBeVisible();
+  // });
+
+  test('login form can be opened', async ({ page }) => {
     await page.goto('http://localhost:5173');
 
-    const locator = await page.getByText('Notes');
-    await expect(locator).toBeVisible();
-    await expect(
-      page.getByText(
-        'Note app, Department of Computer Science, University of Helsinki 2024'
-      )
-    ).toBeVisible();
+    await page.getByRole('button', { name: 'login' }).click();
+
+    await page.getByRole('textbox').first().fill('mluukai');
+    await page.getByRole('textbox').last().fill('salainen');
+    await page.getByRole('button', { name: 'login' }).click();
+
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible();
   });
 });

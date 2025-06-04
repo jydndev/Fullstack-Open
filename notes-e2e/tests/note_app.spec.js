fec31baf193b0b5a91e5c130f65e9f2da6158915
jydnbach <jaydenkim95@gmail.com>
2025-06-04 08:36:15 +0700

    beforeeach to load page

--- notes-e2e/tests/note_app.spec.js
+++ notes-e2e/tests/note_app.spec.js
@@ -1,17 +1,19 @@
 const { test, describe, expect } = require('@playwright/test');
 
 describe('Note app', () => {
-  // test('front page can be opened', async ({ page }) => {
-  //   await page.goto('http://localhost:5173');
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173');
+  });
 
-  //   const locator = await page.getByText('Notes');
-  //   await expect(locator).toBeVisible();
-  //   await expect(
-  //     page.getByText(
-  //       'Note app, Department of Computer Science, University of Helsinki 2024'
-  //     )
-  //   ).toBeVisible();
-  // });
+  test('front page can be opened', async ({ page }) => {
+    const locator = await page.getByText('Notes');
+    await expect(locator).toBeVisible();
+    await expect(
+      page.getByText(
+        'Note app, Department of Computer Science, University of Helsinki 2024'
+      )
+    ).toBeVisible();
+  });
 
   test('login form can be opened', async ({ page }) => {
     await page.goto('http://localhost:5173');
@@ -20,6 +22,7 @@ describe('Note app', () => {
 
     await page.getByTestId('username').fill('mluukai');
     await page.getByTestId('password').fill('salainen');
+    // make sure data exists in db
     await page.getByRole('button', { name: 'login' }).click();
 
     await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible();

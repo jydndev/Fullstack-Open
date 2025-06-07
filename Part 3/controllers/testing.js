f91ae4e8975a07ad542a49ff5d06fb8ff6554b00
jydnbach <jaydenkim95@gmail.com>
2025-06-07 08:38:12 +0700

    change test config

--- Part 3/controllers/testing.js
+++ Part 3/controllers/testing.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
 
-router.post('/reset', async (req, res) => {
+router.post('/reset', async (request, response) => {
   await Note.deleteMany({});
   await User.deleteMany({});
 
-  res.status(204).end();
+  response.status(204).end();
 });
 
 module.exports = router;

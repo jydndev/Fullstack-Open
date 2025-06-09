8d8c6175559a5701015f0f0bdb6871bf1b24173b
jydnbach <jaydenkim95@gmail.com>
2025-06-09 15:38:45 +0700

    main.jsx store.subscribe and re-render

--- Part 6 - redux-note/src/main.jsx
+++ Part 6 - redux-note/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import ReactDOM from 'react-dom/client';
+import { store } from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);

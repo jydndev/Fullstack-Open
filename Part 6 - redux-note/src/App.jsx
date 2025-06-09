ae622e2a09fb1cbadab29e095bcb9fe9be26efa8
jydnbach <jaydenkim95@gmail.com>
2025-06-09 16:58:31 +0700

    reducer path

--- Part 6 - redux-note/src/App.jsx
+++ Part 6 - redux-note/src/App.jsx
@@ -1,45 +1,13 @@
-import { createStore } from 'redux';
+import Notes from './components/Notes';
+import NewNote from './components/NewNote';
 
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    state.push(action.payload);
-    return state;
-  }
-
-  return state;
-};
-
-const store = createStore(noteReducer);
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2,
-  },
-});
-
-export const App = () => {
+const App = () => {
   return (
     <div>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        ))}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   );
 };
-export default noteReducer;
+
+export default App;

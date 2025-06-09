02a1409325099fc23f91fba6860af2197ab35a42
jydnbach <jaydenkim95@gmail.com>
2025-06-09 16:55:21 +0700

    test driven dev setup

--- Part 6 - redux-note/src/App.jsx
+++ Part 6 - redux-note/src/App.jsx
@@ -1,13 +1,45 @@
-import Notes from './components/Notes';
-import NewNote from './components/NewNote';
+import { createStore } from 'redux';
 
-const App = () => {
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.push(action.payload);
+    return state;
+  }
+
+  return state;
+};
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+});
+
+export const App = () => {
   return (
     <div>
-      <NewNote />
-      <Notes />
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
-
-export default App;
+export default noteReducer;
--- Part 6 - redux-note/src/main.jsx
+++ Part 6 - redux-note/src/main.jsx
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import noteReducer from './reducers/noteReducer';
+import noteReducer from './App';
 
 const store = createStore(noteReducer);
 

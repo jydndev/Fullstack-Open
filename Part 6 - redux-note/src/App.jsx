e5cb37aa0d5e971e58c04d25f4febd4be9c38e63
jydnbach <jaydenkim95@gmail.com>
2025-06-11 09:28:22 +0700

    event handlers

--- Part 6 - redux-note/src/App.jsx
+++ Part 6 - redux-note/src/App.jsx
@@ -5,9 +5,26 @@ const generateId = () => {
 };
 
 const App = () => {
-  const addNote = (e) => {};
+  const addNote = (e) => {
+    e.preventDefault();
+    const content = e.target.note.value;
+    e.target.note.value = '';
+    noteReducer.store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    });
+  };
 
-  const toggleImportance = (id) => {};
+  const toggleImportance = (id) => {
+    noteReducer.store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id },
+    });
+  };
 
   return (
     <div>

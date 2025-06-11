cf1b06980788788954fd9aa9a748e95e1494cfea
jydnbach <jaydenkim95@gmail.com>
2025-06-11 09:31:27 +0700

    combine reucer and app.jsx

--- Part 6 - redux-note/src/App.jsx
+++ Part 6 - redux-note/src/App.jsx
@@ -1,15 +1,52 @@
-import noteReducer from '../reducers/noteReducer';
+import { createStore } from 'redux';
 
-const generateId = () => {
-  Number((Math.random() * 1000000).toFixed(0));
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.payload];
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    }
+
+    default:
+      return state;
+  }
 };
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+});
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const App = () => {
   const addNote = (e) => {
     e.preventDefault();
     const content = e.target.note.value;
     e.target.note.value = '';
-    noteReducer.store.dispatch({
+    store.dispatch({
       type: 'NEW_NOTE',
       payload: {
         content,
@@ -20,7 +57,7 @@ const App = () => {
   };
 
   const toggleImportance = (id) => {
-    noteReducer.store.dispatch({
+    store.dispatch({
       type: 'TOGGLE_IMPORTANCE',
       payload: { id },
     });
@@ -33,7 +70,7 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       <ul>
-        {noteReducer.store.getState().map((note) => (
+        {store.getState().map((note) => (
           <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content}
             <strong>{note.important ? 'important' : ''}</strong>

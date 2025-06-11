6c31ddf0d4013410e1de2a90648df46d96733da1
jydnbach <jaydenkim95@gmail.com>
2025-06-11 12:25:53 +0700

    fix import path of noteReducer

--- Part 6 - redux-note/src/main.jsx
+++ Part 6 - redux-note/src/main.jsx
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import noteReducer from './reducers/noteReducer';
+import noteReducer from '../reducers/noteReducer';
 
 const store = createStore(noteReducer);
 

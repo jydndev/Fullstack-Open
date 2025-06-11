62fdca197b9627a09e3a5e8ba776a53af6389281
jydnbach <jaydenkim95@gmail.com>
2025-06-11 11:57:28 +0700

    action creators: separate actions from app

--- Part 6 - redux-note/src/App.jsx
+++ Part 6 - redux-note/src/App.jsx
@@ -41,26 +41,34 @@ store.dispatch({
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id },
+  };
+};
+
 const App = () => {
   const addNote = (e) => {
     e.preventDefault();
     const content = e.target.note.value;
     e.target.note.value = '';
-    store.dispatch({
-      type: 'NEW_NOTE',
-      payload: {
-        content,
-        important: false,
-        id: generateId(),
-      },
-    });
+    store.dispatch(createNote(content));
   };
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: 'TOGGLE_IMPORTANCE',
-      payload: { id },
-    });
+    store.dispatch(toggleImportanceOf(id));
   };
 
   return (

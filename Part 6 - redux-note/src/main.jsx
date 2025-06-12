840bd10513b558325e37263343c47e45e8e2d0f9
jydnbach <jaydenkim95@gmail.com>
2025-06-12 07:44:07 +0700

    combine reducers in main.jsx

--- Part 6 - redux-note/src/main.jsx
+++ Part 6 - redux-note/src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 import { Provider } from 'react-redux';
+import filterReducer from '../reducers/filterReducer';
 
 import App from './App';
 import noteReducer from '../reducers/noteReducer';
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  note: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>

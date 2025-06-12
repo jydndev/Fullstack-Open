55beb2e37f519589212460eafa2a42bccb4acf09
jydnbach <jaydenkim95@gmail.com>
2025-06-12 15:59:10 +0700

    migrate fetch logic to app use effect for rendering

--- Part 6 - redux-note/src/App.jsx
+++ Part 6 - redux-note/src/App.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { setNotes } from './reducers/noteReducer';
+
 import NewNote from './components/NewNote';
 import Notes from './components/Notes';
 import VisibilityFilter from './components/VisibilityFilter';
 
+import noteService from './services/notes';
+
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    noteService.getAll().then((notes) => dispatch(setNotes(notes)));
+  }, [dispatch]);
+
   return (
     <div>
       <NewNote />
--- Part 6 - redux-note/src/main.jsx
+++ Part 6 - redux-note/src/main.jsx
@@ -1,10 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import App from './App';
+import noteReducer from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer';
-import noteService from './services/notes';
-import noteReducer, { setNotes } from './reducers/noteReducer';
 
 const store = configureStore({
   reducer: {
@@ -12,8 +12,6 @@ const store = configureStore({
     filter: filterReducer,
   },
 });
-
-noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>

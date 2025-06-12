4097bfc8631bdd6ce384a879488552cf4dd41745
jydnbach <jaydenkim95@gmail.com>
2025-06-12 15:52:06 +0700

    set notes instead of append notes

--- Part 6 - redux-note/src/main.jsx
+++ Part 6 - redux-note/src/main.jsx
@@ -2,10 +2,9 @@ import ReactDOM from 'react-dom/client';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import App from './App';
-import noteReducer from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer';
 import noteService from './services/notes';
-import noteReducer, { appendNote } from './reducers/noteReducer';
+import noteReducer, { setNotes } from './reducers/noteReducer';
 
 const store = configureStore({
   reducer: {
@@ -14,11 +13,7 @@ const store = configureStore({
   },
 });
 
-noteService.getAll().then((notes) =>
-  notes.forEach((note) => {
-    store.dispatch(appendNote(note));
-  })
-);
+noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>

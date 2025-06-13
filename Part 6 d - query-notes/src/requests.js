cc0529572d6e4ce433467778e2a610764bfdf938
jydnbach <jaydenkim95@gmail.com>
2025-06-13 14:42:29 +0700

    fix requests

--- Part 6 d - query-notes/src/requests.js
+++ Part 6 d - query-notes/src/requests.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:3001/notes';
 
-export const getNotes = () => axios.get().then((res) => res.data);
+export const getNotes = () => axios.get(baseUrl).then((res) => res.data);
 
-export const createNote = (newNote) => {
+export const createNote = (newNote) =>
   axios.post(baseUrl, newNote).then((res) => res.data);
-};
 
 export const updateNote = (updatedNote) =>
-  axios.put(`${baseUrl}/${updatedNote.id}`).then((res) => res.data);
+  axios
+    .put(`${baseUrl}/${updatedNote.id}`, updatedNote)
+    .then((res) => res.data);

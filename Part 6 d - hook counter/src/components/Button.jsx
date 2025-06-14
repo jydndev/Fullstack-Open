6ba212fdf590b5b9a68523cc402d8bf10dbff5f2
jydnbach <jaydenkim95@gmail.com>
2025-06-14 07:23:54 +0700

    usecontext in child components

--- Part 6 d - hook counter/src/components/Button.jsx
+++ Part 6 d - hook counter/src/components/Button.jsx
@@ -1,4 +1,8 @@
+import { useContext } from 'react';
+import CounterContext from '../CounterContext';
+
 const Button = ({ dispatch, type, label }) => {
+  const [counter, dispatch] = useContext(CounterContext);
   return <button onClick={() => dispatch({ type })}>{label}</button>;
 };
 
--- Part 6 d - hook counter/src/components/Display.jsx
+++ Part 6 d - hook counter/src/components/Display.jsx
@@ -1,4 +1,8 @@
+import { useContext } from 'react';
+import CounterContext from '../CounterContext';
+
 const Display = ({ counter }) => {
+  const [counter] = useContext(CounterContext);
   return <div>{counter}</div>;
 };
 

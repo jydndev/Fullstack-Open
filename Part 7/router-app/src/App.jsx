0d6d7188c83faa54e39c75f0bd60495ba0bb771c
jydnbach <jaydenkim95@gmail.com>
2025-06-15 07:39:12 +0700

    app router setup

--- Part 7/router-app/src/App.jsx
+++ Part 7/router-app/src/App.jsx
@@ -1,12 +1,32 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
+  const padding = {
+    padding: 5,
+  };
+
   return (
     <Router>
       <div>
-        <Link>home</Link>
-        <Link>notes</Link>
-        <Link>users</Link>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="users/">
+          users
+        </Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science</i>
       </div>
     </Router>
   );

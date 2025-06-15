028869649b50054796ebf069722b82f1e1a897a5
jydnbach <jaydenkim95@gmail.com>
2025-06-15 07:58:53 +0700

    fix error notes props div wrapper

--- Part 7/router-app/src/App.jsx
+++ Part 7/router-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Home = () => (
   <div>
@@ -81,34 +82,57 @@ const Login = (props) => {
 };
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas',
+    },
+  ]);
+
   const padding = {
     padding: 5,
   };
 
   return (
-    <Router>
-      <div>
-        <Link style={padding} to="/">
-          home
-        </Link>
-        <Link style={padding} to="/notes">
-          notes
-        </Link>
-        <Link style={padding} to="users/">
-          users
-        </Link>
-      </div>
+    <div>
+      <Router>
+        <div>
+          <Link style={padding} to="/">
+            home
+          </Link>
+          <Link style={padding} to="/notes">
+            notes
+          </Link>
+          <Link style={padding} to="/users">
+            users
+          </Link>
+        </div>
 
-      <Routes>
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path="/notes" element={<Notes notes={notes} />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
 
       <div>
         <i>Note app, Department of Computer Science</i>
       </div>
-    </Router>
+    </div>
   );
 };
 

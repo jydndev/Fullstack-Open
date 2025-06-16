3557db7f68212f6be4692c3b7b258ea2be6392c1
jydnbach <jaydenkim95@gmail.com>
2025-06-16 07:41:16 +0700

    login bootstrap Alert settimeout

--- Part 7/router-app/src/App.jsx
+++ Part 7/router-app/src/App.jsx
@@ -9,7 +9,7 @@ import {
 } from 'react-router-dom';
 import { useState } from 'react';
 
-import { Table, Form, Button } from 'react-bootstrap';
+import { Table, Form, Button, Alert } from 'react-bootstrap';
 
 const Home = () => (
   <div>
@@ -121,9 +121,14 @@ const App = () => {
   ]);
 
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState(null);
 
   const login = (user) => {
     setUser(user);
+    setMessage(`Welcome ${user}`);
+    setTimeout(() => {
+      setMessage(null);
+    }, 10000);
   };
 
   const match = useMatch('/notes/:id');
@@ -138,6 +143,7 @@ const App = () => {
 
   return (
     <div className="container">
+      {message && <Alert variant="success">{message}</Alert>}
       <div>
         <Link style={padding} to="/">
           home

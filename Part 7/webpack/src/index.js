615020043bf49cdc980b7926998c5a98eca1a7e6
jydnbach <jaydenkim95@gmail.com>
2025-06-17 07:38:05 +0700

    fix async/await bundling error

--- Part 7/webpack/src/index.js
+++ Part 7/webpack/src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// fix async/await bundling error
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);

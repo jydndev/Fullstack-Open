5f8de21c53bbb77322bcdc0a11e2fb4cf828ac56
jydnbach <jaydenkim95@gmail.com>
2025-06-20 16:55:52 +0700

    create person query

--- Part 8/graphql-phonebook-frontend/src/queries.js
+++ Part 8/graphql-phonebook-frontend/src/queries.js
@@ -10,6 +10,25 @@ export const ALL_PERSONS = gql`
   }
 `;
 
+export const CREATE_PERSON = gql`
+  mutation createPerson(
+    $name: String!
+    $street: String!
+    $city: String!
+    $phone: String
+  ) {
+    addPerson(name: $name, street: $street, city: $city, phone: $phone) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
+
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {

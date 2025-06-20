7fca34aa2fc38bff5f47a4e8839c40b5430c5e31
jydnbach <jaydenkim95@gmail.com>
2025-06-20 16:50:48 +0700

    add PersonForm

---
+++ Part 8/graphql-phonebook-frontend/src/components/PersonForm.jsx
@@ -0,0 +1,21 @@
+
+import {} from 
+
+const PersonForm = () => {
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={}>
+
+
+      <div>name 
+        <input value={} onChange={} />
+      </div>
+      <button type="submit">add</button>
+      </form>
+    </div>
+  )
+};
+
+export default PersonForm;

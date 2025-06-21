f7c6fa93d0a68c62141243488ef6ac1af475d0f3
jydnbach <jaydenkim95@gmail.com>
2025-06-21 09:57:47 +0700

    set error message state change send prop to notify

--- Part 8/graphql-phonebook-frontend/src/App.jsx
+++ Part 8/graphql-phonebook-frontend/src/App.jsx
@@ -1,21 +1,32 @@
 import { useQuery } from '@apollo/client';
-
 import { ALL_PERSONS } from './queries';
 
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
+import Notify from './components/Notify';
+
+import { useState } from 'react';
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 };

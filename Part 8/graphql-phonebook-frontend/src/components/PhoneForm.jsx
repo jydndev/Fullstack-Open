9cde86a04820a143d793bebfef3119948704c2aa
jydnbach <jaydenkim95@gmail.com>
2025-06-22 06:51:55 +0700

    phone form component

--- Part 8/graphql-phonebook-frontend/src/components/PhoneForm.jsx
+++ Part 8/graphql-phonebook-frontend/src/components/PhoneForm.jsx
@@ -1,3 +1,45 @@
-const PhoneForm = () => {};
+import { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+
+import { EDIT_NUMBER } from '../queries';
+
+const PhoneForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const [changeNumber] = useMutation(EDIT_NUMBER);
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    changeNumber({ variables: { name, number } });
+
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          number
+          <input
+            value={number}
+            onChange={({ target }) => setNumber(target.value)}
+          />
+        </div>
+      </form>
+      <button type="submit">submit</button>
+    </div>
+  );
+};
 
 export default PhoneForm;

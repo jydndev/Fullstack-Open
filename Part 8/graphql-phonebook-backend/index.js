fa9c437b23a743dec73f67ce66c2d52d75a51df8
jydnbach <jaydenkim95@gmail.com>
2025-06-23 07:05:54 +0700

    mongodb logic in graphql resolvers
    comment out persons array

--- Part 8/graphql-phonebook-backend/index.js
+++ Part 8/graphql-phonebook-backend/index.js
@@ -22,28 +22,28 @@ mongoose
     console.log('error connection to MongoDB:', err.message);
   });
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    phone: '040-123543',
-    street: 'Tapiolankatu 5 A',
-    city: 'Espoo',
-    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Matti Luukkainen',
-    phone: '040-432342',
-    street: 'Malminkaari 10 A',
-    city: 'Helsinki',
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Venla Ruuska',
-    street: 'Nallemäentie 22 C',
-    city: 'Helsinki',
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
-  },
-];
+// let persons = [
+//   {
+//     name: 'Arto Hellas',
+//     phone: '040-123543',
+//     street: 'Tapiolankatu 5 A',
+//     city: 'Espoo',
+//     id: '3d594650-3436-11e9-bc57-8b80ba54c431',
+//   },
+//   {
+//     name: 'Matti Luukkainen',
+//     phone: '040-432342',
+//     street: 'Malminkaari 10 A',
+//     city: 'Helsinki',
+//     id: '3d599470-3436-11e9-bc57-8b80ba54c431',
+//   },
+//   {
+//     name: 'Venla Ruuska',
+//     street: 'Nallemäentie 22 C',
+//     city: 'Helsinki',
+//     id: '3d599471-3436-11e9-bc57-8b80ba54c431',
+//   },
+// ];
 
 const typeDefs = `
   type Address {
@@ -92,16 +92,20 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
+    personCount: async () => Person.collection.countDocuments(),
     allPersons: (root, args) => {
-      if (!args.phone) {
-        return persons;
-      }
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone;
-      return persons.filter(byPhone);
+      // if (!args.phone) {
+      //   return persons;
+      // }
+      // const byPhone = (person) =>
+      //   args.phone === 'YES' ? person.phone : !person.phone;
+      // return persons.filter(byPhone);
+
+      // filter missing
+
+      return Person.find({});
     },
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+    findPerson: (root, args) => Person.findOne({ name: args.name }),
   },
   Person: {
     address: ({ street, city }) => {
@@ -112,28 +116,35 @@ const resolvers = {
     },
   },
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find((p) => p.name === args.name)) {
-        throw new GraphQLError('Name must be unique', {
-          extensions: {
-            code: 'BAD_USER_INPUT',
-            invalidArgs: args.name,
-          },
-        });
-      }
-      const person = { ...args, id: uuid() };
-      persons = persons.concat(person);
-      return person;
-    },
-    editNumber: (root, args) => {
-      const person = persons.find((p) => p.name === args.name);
-      if (!person) {
-        return null;
-      }
+    addPerson: async (root, args) => {
+      // if (persons.find((p) => p.name === args.name)) {
+      //   throw new GraphQLError('Name must be unique', {
+      //     extensions: {
+      //       code: 'BAD_USER_INPUT',
+      //       invalidArgs: args.name,
+      //     },
+      //   });
+      // }
+      // const person = { ...args, id: uuid() };
+      // persons = persons.concat(person);
+      // return person;
 
-      const updatedPerson = { ...person, phone: args.phone };
-      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
-      return updatedPerson;
+      const person = new Person({ ...args });
+      return person.save();
+    },
+    editNumber: async (root, args) => {
+      // const person = persons.find((p) => p.name === args.name);
+      // if (!person) {
+      //   return null;
+      // }
+
+      // const updatedPerson = { ...person, phone: args.phone };
+      // persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+      // return updatedPerson;
+
+      const person = await Person.findOne({ name: args.name });
+      person.phone = args.phone;
+      return person.save();
     },
   },
 };

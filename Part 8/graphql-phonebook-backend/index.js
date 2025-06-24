4f978a746edcbc63ac02b45673ecf731837fb47d
jydnbach <jaydenkim95@gmail.com>
2025-06-24 13:59:16 +0700

    add context to backend startup. return currentUser as 3rd param for user identification.

--- Part 8/graphql-phonebook-backend/index.js
+++ Part 8/graphql-phonebook-backend/index.js
@@ -106,7 +106,7 @@ const typeDefs = `
       username: String!
     ): User
     
-    login:(
+    login(
       username: String!
       password: String!
     ): Token
@@ -213,6 +213,19 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7),
+        process.env.JWT_SECRET
+      );
+      const currentUser = await User.findById(decodedToken.id).populate(
+        'friends'
+      );
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });

64b319e73bbb40067a015a3c5587770e6f461427
jydnbach <jaydenkim95@gmail.com>
2025-06-25 07:28:07 +0700

    sanitize data on logout

--- Part 8/graphql-phonebook-frontend/src/App.jsx
+++ Part 8/graphql-phonebook-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@apollo/client';
+import { useApolloClient, useQuery } from '@apollo/client';
 import { ALL_PERSONS } from './queries';
 
 import { useState } from 'react';
@@ -12,6 +12,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
   const [token, setToken] = useState(null);
+  const client = useApolloClient();
 
   if (result.loading) {
     return <div>loading...</div>;
@@ -24,19 +25,25 @@ const App = () => {
     }, 10000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
   if (!token) {
     return (
-      <div>
+      <>
         <Notify errorMessage={errorMessage} />
-        <h2>Login</h2>
         <LoginForm setToken={setToken} setError={notify} />
-      </div>
+      </>
     );
   }
 
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />

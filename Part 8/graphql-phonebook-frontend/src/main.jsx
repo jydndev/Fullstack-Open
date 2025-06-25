67b41aa940888665d4d77301f4448dc4c22fc64b
jydnbach <jaydenkim95@gmail.com>
2025-06-25 08:27:54 +0700

    delete query in main.jsx delete console log for client query in main.jsx

--- Part 8/graphql-phonebook-frontend/src/main.jsx
+++ Part 8/graphql-phonebook-frontend/src/main.jsx
@@ -5,46 +5,27 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink
-  gql,
+  createHttpLink,
 } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context'
+import { setContext } from '@apollo/client/link/context';
 
-const authLink = setContext((_, {headers}) => {
-  const token = localStorage.getItem('phonenumbers-user-token')
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('phonenumbers-user-token');
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : null
-    }
-  }
-}) 
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
-})
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
-});
-
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`;
-
-client.query({ query }).then((response) => {
-  console.log(response.data);
+  link: authLink.concat(httpLink),
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(

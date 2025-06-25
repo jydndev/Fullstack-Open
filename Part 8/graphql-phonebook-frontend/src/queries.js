b1a60d267e43589fa5671ca70c3c4d3a18e6d93d
jydnbach <jaydenkim95@gmail.com>
2025-06-25 08:43:36 +0700

    fix login mutation syntax

--- Part 8/graphql-phonebook-frontend/src/queries.js
+++ Part 8/graphql-phonebook-frontend/src/queries.js
@@ -59,7 +59,8 @@ export const EDIT_NUMBER = gql`
 
 export const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
-    login(username: $username, password: $password)
+    login(username: $username, password: $password) {
     value
+    }
   }
 `;

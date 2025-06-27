9e85a7a79927701ef781d062c11fb9f19edbe460
jydnbach <jaydenkim95@gmail.com>
2025-06-27 08:12:19 +0700

    creater apollo server server listen to port 4000 app.use block for express middleware

--- Part 8/graphql-phonebook-backend/index.js
+++ Part 8/graphql-phonebook-backend/index.js
@@ -54,6 +54,24 @@ mongoose
 // });
 
 // graphql server starts before express
-const start = async () => {};
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  app.use('/', cors(), express.json(), expressMiddleware(server, {}));
+
+  const PORT = 4000;
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server is now running on http://localhost:${PORT}`);
+  });
+};
 
 start();

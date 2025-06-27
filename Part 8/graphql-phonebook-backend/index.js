832b202758341f8f5481b25120c568bdef14545e
jydnbach <jaydenkim95@gmail.com>
2025-06-27 08:01:59 +0700

    imports index — setup for express middleware

--- Part 8/graphql-phonebook-backend/index.js
+++ Part 8/graphql-phonebook-backend/index.js
@@ -1,9 +1,24 @@
 const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+// const { startStandaloneServer } = require('@apollo/server/standalone');
+const { expressMiddleware } = require('@apollo/server/express4');
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+const jwt = require('jsonwebtoken');
 
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
 
+const User = require('./models/user');
+
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+mongoose.set('strictQuery', false);
 require('dotenv').config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -19,28 +34,28 @@ mongoose
     console.log('error connection to MongoDB:', error.message);
   });
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    phone: '040-123543',
-    street: 'Tapiolankatu 5 A',
-    city: 'Espoo',
-    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Matti Luukkainen',
-    phone: '040-432342',
-    street: 'Malminkaari 10 A',
-    city: 'Helsinki',
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Venla Ruuska',
-    street: 'Nallemäentie 22 C',
-    city: 'Helsinki',
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
-  },
-];
+// let persons = [
+//   {
+//     name: 'Arto Hellas',
+//     phone: '040-123543',
+//     street: 'Tapiolankatu 5 A',
+//     city: 'Espoo',
+//     id: '3d594650-3436-11e9-bc57-8b80ba54c431',
+//   },
+//   {
+//     name: 'Matti Luukkainen',
+//     phone: '040-432342',
+//     street: 'Malminkaari 10 A',
+//     city: 'Helsinki',
+//     id: '3d599470-3436-11e9-bc57-8b80ba54c431',
+//   },
+//   {
+//     name: 'Venla Ruuska',
+//     street: 'Nallemäentie 22 C',
+//     city: 'Helsinki',
+//     id: '3d599471-3436-11e9-bc57-8b80ba54c431',
+//   },
+// ];
 
 const server = new ApolloServer({
   typeDefs,

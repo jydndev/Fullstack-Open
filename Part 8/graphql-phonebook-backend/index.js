9e2be6903d65a69b0b4b473102bdfe14b32eda20
jydnbach <jaydenkim95@gmail.com>
2025-06-27 08:24:12 +0700

    organize imports in indexjs

--- Part 8/graphql-phonebook-backend/index.js
+++ Part 8/graphql-phonebook-backend/index.js
@@ -4,23 +4,27 @@ const {
   ApolloServerPluginDrainHttpServer,
 } = require('@apollo/server/plugin/drainHttpServer');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
+
+const http = require('http');
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 
 const jwt = require('jsonwebtoken');
 
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
 const User = require('./models/user');
+const Person = require('./models/person');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-// mongodb
-mongoose.set('strictQuery', false);
 require('dotenv').config();
 
+// mongodb
 const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log('connecting to', MONGODB_URI);
@@ -33,25 +37,6 @@ mongoose
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message);
   });
-
-// startStandaloneServer(server, {
-//   listen: { port: 4000 },
-//   context: async ({ req, res }) => {
-//     const auth = req ? req.headers.authorization : null;
-//     if (auth && auth.startsWith('Bearer ')) {
-//       const decodedToken = jwt.verify(
-//         auth.substring(7),
-//         process.env.JWT_SECRET
-//       );
-//       const currentUser = await User.findById(decodedToken.id).populate(
-//         'friends'
-//       );
-//       return { currentUser };
-//     }
-//   },
-// }).then(({ url }) => {
-//   console.log(`Server ready at ${url}`);
-// });
 
 // graphql server starts before express
 const start = async () => {

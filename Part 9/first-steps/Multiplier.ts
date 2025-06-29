3bd1e9b5476a1baf8867363b3b56b2cb1da73711
jydnbach <jaydenkim95@gmail.com>
2025-06-30 06:46:52 +0700

    type Operation fn calc

--- Part 9/first-steps/Multiplier.ts
+++ Part 9/first-steps/Multiplier.ts
@@ -1,5 +1,14 @@
 // npm run ts-node -- multiplier.ts
-const multiplicator = (a: number, b: number, printText: string) => {
-  console.log(printText, a * b);
+
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation) => {
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) return `can't divide by 0`;
+    return a / b;
+  }
 };
-multiplicator(2, 4, 'multiplied numbers 2 and 4, the result is:');

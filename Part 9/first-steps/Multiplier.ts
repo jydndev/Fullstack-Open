4dc1d125571c14cf05c90701d98d0c495404a7f1
jydnbach <jaydenkim95@gmail.com>
2025-06-29 08:33:48 +0700

    add types

--- Part 9/first-steps/Multiplier.ts
+++ Part 9/first-steps/Multiplier.ts
@@ -1,5 +1,5 @@
 // npm run ts-node -- multiplier.ts
-const multiplicator = (a, b, printText) => {
+const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
 };
 multiplicator(2, 4, 'multiplied numbers 2 and 4, the result is:');

08cc08b2d8c48d08f912a654b80d940c7292c3a0
jydnbach <jaydenkim95@gmail.com>
2025-06-29 08:31:07 +0700

    multiplicator fn and log

--- Part 9/first-steps/Multiplier.ts
+++ Part 9/first-steps/Multiplier.ts
@@ -1,1 +1,5 @@
 // npm run ts-node -- multiplier.ts
+const multiplicator = (a, b, printText) => {
+  console.log(printText, a * b);
+};
+multiplicator(2, 4, 'multiplied numbers 2 and 4, the result is:');

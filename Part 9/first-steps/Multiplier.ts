c01878e27a75e6fb02b1c52f608e3c7fdb76ba45
jydnbach <jaydenkim95@gmail.com>
2025-06-30 07:04:01 +0700

    switch statement error block

--- Part 9/first-steps/Multiplier.ts
+++ Part 9/first-steps/Multiplier.ts
@@ -2,13 +2,19 @@
 
 type Operation = 'multiply' | 'add' | 'divide';
 
-const calculator = (a: number, b: number, op: Operation) => {
-  if (op === 'multiply') {
-    return a * b;
-  } else if (op === 'add') {
-    return a + b;
-  } else if (op === 'divide') {
-    if (b === 0) return `can't divide by 0`;
-    return a / b;
+const calculator = (a: number, b: number, op: Operation): number => {
+  switch (op) {
+    case 'multiply':
+      return a * b;
+    case 'add':
+      return a + b;
+    case 'divide':
+      if (b === 0) return `can't divide by 0`;
+      return a / b;
+    default:
+      throw new Error('Operation is not multiply, add, or divide');
   }
 };
+
+try {
+} catch (error: unkonwn) {}

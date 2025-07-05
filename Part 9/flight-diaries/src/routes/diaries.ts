d8b9a5f6a9021575191f7f281cce260f55b223ca
jydnbach <jaydenkim95@gmail.com>
2025-07-05 09:02:50 +0700

    diary add diary route handler: try catch block apply to new diary entry function to avoid error disable eslint ignore rule

--- Part 9/flight-diaries/src/routes/diaries.ts
+++ Part 9/flight-diaries/src/routes/diaries.ts
@@ -1,23 +1,25 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 const router = express.Router();
 
 import diaryService from '../services/diaryService';
+import toNewDiaryEntry from '../utils';
 
 router.get('/', (_req, res) => {
   res.send(diaryService.getNonSensitiveEntires());
 });
 
 router.post('/', (req, res) => {
-  const { date, weather, visibility, comment } = req.body;
-  const addedEntry = diaryService.addDiary({
-    date,
-    weather,
-    visibility,
-    comment,
-  });
-
-  res.json(addedEntry);
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 router.get('/:id', (req, res) => {

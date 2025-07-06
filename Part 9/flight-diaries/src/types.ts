e8b7233fd72acd024f4f68ec331aaa2e6714a1fb
jydnbach <jaydenkim95@gmail.com>
2025-07-06 08:26:22 +0700

    visibility enum

--- Part 9/flight-diaries/src/types.ts
+++ Part 9/flight-diaries/src/types.ts
@@ -6,7 +6,12 @@ export enum Weather {
   Stormy = 'stormy',
 }
 
-export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
 
 export interface DiaryEntry {
   id: number;

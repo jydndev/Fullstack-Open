5725aa2bfc04a5487e05fb96067b12bbe35572e5
jydnbach <jaydenkim95@gmail.com>
2025-07-08 07:55:43 +0700

    interfaces for course parts

--- Part 9/flight-diaries-frontend/src/App.tsx
+++ Part 9/flight-diaries-frontend/src/App.tsx
@@ -1,3 +1,26 @@
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: 'basic';
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: 'group';
+}
+
+interface CoursePartBackground extends CoursePartBase {
+  description: string;
+  backgroundMaterial: string;
+  kind: 'background';
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
 function App() {
   const courseName = 'application dev';
   const courseParts: CoursePart[] = [

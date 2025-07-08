572a833f79764fde1a81780393fc9f099c74217c
jydnbach <jaydenkim95@gmail.com>
2025-07-08 07:52:48 +0700

    kind attribute in course parts data

--- Part 9/flight-diaries-frontend/src/App.tsx
+++ Part 9/flight-diaries-frontend/src/App.tsx
@@ -1,19 +1,23 @@
 function App() {
-  const courseParts = [
+  const courseName = 'application dev';
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
       description: 'This is an awesome course part',
+      kind: 'basic',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
       groupProjectCount: 3,
+      kind: 'group',
     },
     {
       name: 'Basics of type Narrowing',
       exerciseCount: 7,
       description: 'How to go from unknown to string',
+      kind: 'basic',
     },
     {
       name: 'Deeper type usage',
@@ -21,6 +25,7 @@ function App() {
       description: 'Confusing description',
       backgroundMaterial:
         'https://type-level-typescript.com/template-literal-types',
+      kind: 'background',
     },
   ];
 

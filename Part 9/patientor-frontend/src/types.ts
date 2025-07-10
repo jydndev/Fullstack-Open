28493ad5764805bcb115749c342e363dd340c332
jydnbach <jaydenkim95@gmail.com>
2025-07-10 07:22:32 +0700

    healthcheckentry (extends baseentry)

--- Part 9/patientor-frontend/src/types.ts
+++ Part 9/patientor-frontend/src/types.ts
@@ -21,10 +21,27 @@ export interface Patient {
 
 export type PatientFormValues = Omit<Patient, 'id' | 'entries'>;
 
-export interface BaseEntry {
+interface BaseEntry {
   id: string;
   description: string;
   date: string;
   specialist: string;
   diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
